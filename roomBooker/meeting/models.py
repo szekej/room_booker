5ee62519389cf41b05aec740439b1916ece0b839
random name <testmai@gmail.com>
2023-06-14 20:56:06 +0200

    function-based-view

--- roomBooker/meeting/models.py
+++ roomBooker/meeting/models.py
@@ -4,7 +4,10 @@ from django.db import models
 # Create your models here.
 
 class Room(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True, default="")
+    floor = models.IntegerField()
+    capacity = models.IntegerField()
+    description = models.TextField(max_length=200)
 
     def __str__(self):
         return self.name
