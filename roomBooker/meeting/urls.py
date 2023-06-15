b0e36e61e52019b3f096b7a5081248a9b1674e09
random name <testmai@gmail.com>
2023-06-15 22:11:20 +0200

    class-based-view

--- roomBooker/meeting/urls.py
+++ roomBooker/meeting/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import welcome, showRoom, new, detail
+from .views import WelcomeView, ShowRoomView, RoomListView, CreateRoomView
 
 
 urlpatterns = [
-    path('welcome/', welcome),
-    path('rooms/', showRoom, name='rooms'),
-    path('new/', new, name='add_room'),
-    path('rooms/<int:room_id>', detail, name='room_detail')
+    path('welcome/', WelcomeView.as_view(), name='welcome'),
+    path('rooms/', RoomListView.as_view(), name='rooms'),
+    path('new/', CreateRoomView.as_view(), name='add_room'),
+    path('rooms/<int:pk>', ShowRoomView.as_view(), name='room_detail')
 ]
